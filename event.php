4b948ef7d726c6a95ffd6ba6d716be4586dc1062
valbeat <kj1ktk@gmail.com>
2015-11-28 20:06:45 +0900

    ダミー表示

---
+++ event.php
@@ -0,0 +1,41 @@
+<?php
+require_once('config.php');
+require_once('functions.php');
+
+mb_language("uni");
+mb_internal_encoding("utf-8"); //内部文字コードを変更
+mb_http_input("auto");
+mb_http_output("utf-8");
+
+$dbh = connectDb();
+
+//ページの管理
+if (preg_match('/^[1-9][0-9]*$/', $_GET['num'])) {
+    $page = (int)$_GET['num'];
+} else {
+    $page = 1;
+}
+
+//GETメソッドで受け取った値によって動作を切り替え
+if (!$_GET) {
+  $sql = "SELECT * FROM `events`";
+}
+
+$data = array();
+if($dbh->query($sql)) {
+  foreach($dbh->query($sql) as $row) {
+    $data[] = array(
+      'event_name'=> $row['event_name'],
+      'date'=> $row['start_date'],
+      'place'=> $row['address'],
+      'tag'=> "アウトドア",
+      'pictureurl'=> 'http://placehold.jp/150x150.png'
+    );
+  }
+} else {
+  $infos = null;
+}
+header('Access-Control-Allow-Origin:*');
+header('Content-type: application/json');
+exit(json_encode($data));
+?>
