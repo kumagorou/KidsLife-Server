bebe7bd78f9a1976884f1f7ecdf9585c6b233cc7
valbeat <kj1ktk@gmail.com>
2015-11-28 21:22:44 +0900

    id指定に対応

--- event.php
+++ event.php
@@ -16,25 +16,48 @@ if (preg_match('/^[1-9][0-9]*$/', $_GET['num'])) {
     $page = 1;
 }
 
+
 //GETメソッドで受け取った値によって動作を切り替え
-if (!$_GET) {
-  $sql = "SELECT * FROM `events`";
+if (preg_match('/^[1-9][0-9]*$/', $_GET['id'])) {
+    $id = (int)$_GET['id'];
+    $sql = "SELECT * FROM `events` WHERE `id` = $id LIMIT 1";
+    if($dbh->query($sql)) {
+      foreach($dbh->query($sql) as $row) {
+        $data[] = array(
+          'id'=> $row['id'],
+          'event_name'=> $row['event_name'],
+          'date'=> $row['start_date'],
+          'place'=> $row['address'],
+          'tag'=> "アウトドア",
+          'pictureurl'=> 'http://placehold.jp/150x150.png'
+        );
+      }
+    } else {
+      $data = null;
+    }
+} else if($_GET['id']) {
+    $data = null;
 }
 
-$data = array();
-if($dbh->query($sql)) {
-  foreach($dbh->query($sql) as $row) {
-    $data[] = array(
-      'event_name'=> $row['event_name'],
-      'date'=> $row['start_date'],
-      'place'=> $row['address'],
-      'tag'=> "アウトドア",
-      'pictureurl'=> 'http://placehold.jp/150x150.png'
-    );
+if (!$_GET) {
+  $sql = "SELECT * FROM `events`";
+  $data = array();
+  if($dbh->query($sql)) {
+    foreach($dbh->query($sql) as $row) {
+      $data[] = array(
+        'id'=> $row['id'],
+        'event_name'=> $row['event_name'],
+        'date'=> $row['start_date'],
+        'place'=> $row['address'],
+        'tag'=> "アウトドア",
+        'pictureurl'=> 'http://placehold.jp/150x150.png'
+      );
+    }
+  } else {
+    $data = null;
   }
-} else {
-  $infos = null;
 }
+
 header('Access-Control-Allow-Origin:*');
 header('Content-type: application/json');
 exit(json_encode($data));
